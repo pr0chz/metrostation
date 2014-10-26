52432b73c6dcefbdc73927e7f05d9f29cc7598cc
pr0chy <pystole@gmail.com>
2014-10-26 18:11:50 +0100

    Switched main service to use new code

--- src/cz/prochy/metrostation/NotificationService.java
+++ src/cz/prochy/metrostation/NotificationService.java
@@ -23,62 +23,12 @@ import android.widget.Toast;
 
 public class NotificationService extends Service {
 	
-	private final static String LOG_NAME = "MetroNotifier";
-	
-	private int currentCellId;
-	private AtomicBoolean notified = new AtomicBoolean();
-	
-	private ScheduledExecutorService scheduledService = Executors.newSingleThreadScheduledExecutor();
-	private Future<?> scheduledTask;
-	
 	private static final Stations stations = new Stations();
-	
-	private static final int NOTIFICATION_ID = 0xbadbeef;
-	
-	private final Runnable cancelNotificationTask = new Runnable() {
-		public void run() {
-			NotificationManagerCompat.from(NotificationService.this).cancel(NOTIFICATION_ID);
-			notified.set(false);
-		};
-	};
-	
-	private void rescheduleCancelNotification() {
-		if (scheduledTask != null) {
-			scheduledTask.cancel(false);
-		}
-		scheduledTask = scheduledService.schedule(cancelNotificationTask, 180, TimeUnit.SECONDS);
-	}
-	
-	private void connected(int cellId) {
-		if (stations.isStation(cellId) && notified.compareAndSet(false, true)) {
-			String name = stations.getName(cellId);
-			Log.v(LOG_NAME, "Matched! Notifying... " + name);
-			notifyStation(name, !name.equals(stations.getName(currentCellId)), true);
-		}
-		currentCellId = cellId;
-	}
-	
-	private void disconnected() {
-		if (notified.compareAndSet(true, false) && stations.isStation(currentCellId)) {
-			notifyStation(stations.getName(currentCellId), false, false);
-		}
-	}
-	
-	private void notifyStation(String stationName, boolean toast, boolean connected) {
-		rescheduleCancelNotification();
-		
-		if (toast) {
-			Toast.makeText(this, stationName, Toast.LENGTH_SHORT).show();
-		}
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
-        	.setSmallIcon(R.drawable.ic_launcher)
-        	.setContentTitle("Metro station")
-        	.setContentText(stationName + (connected ? "" : " -> ???"))
-        	.setAutoCancel(true);
- 
-        NotificationManagerCompat.from(this).notify(NOTIFICATION_ID, builder.build());
-	}
+	private final static String LOG_NAME = "MetroNotifier";
+	private final ScheduledExecutorService scheduledService = Executors.newSingleThreadScheduledExecutor();
+	
+	private CellListener rootListener;
 
 	private class StateListener extends PhoneStateListener {
 		@Override
@@ -87,7 +37,7 @@ public class NotificationService extends Service {
 			switch (serviceState.getState()) {
 			case ServiceState.STATE_OUT_OF_SERVICE:
 				Log.v(LOG_NAME, "Disconnected");
-				disconnected();
+				rootListener.disconnected();
 				break;
 			case ServiceState.STATE_EMERGENCY_ONLY:
 			case ServiceState.STATE_IN_SERVICE:
@@ -99,25 +49,34 @@ public class NotificationService extends Service {
 						int cellId = -1;
 						if (cl instanceof GsmCellLocation) {
 							GsmCellLocation gcl = (GsmCellLocation) cl;
-							cellId = gcl.getCid();
+							rootListener.cellInfo(gcl.getCid(), gcl.getLac());
 						} else if (cl instanceof CdmaCellLocation) {
 							CdmaCellLocation ccl = (CdmaCellLocation) cl;
-							cellId = ccl.getBaseStationId();
-						}
-						
-						Log.v(LOG_NAME, "Connected to " + cellId);
-						if (cellId != -1) {
-							connected(cellId);
+							rootListener.cellInfo(ccl.getBaseStationId(), -1);
 						}
 					}
 				}
 				break;
 			default:
 				Log.v(LOG_NAME, "Other state");
-				disconnected();
+				rootListener.disconnected();
 			}
 			super.onServiceStateChanged(serviceState);
 		}
+	}
+
+	private CellListener buildListeners() {
+		CompositeStationListener compositeStationListener = new CompositeStationListener();
+		StationsCellListener stationsCellListener = new StationsCellListener(stations, compositeStationListener);
+		CellListener rootListener = new CellListenerFilter(stationsCellListener);
+		
+		Notifications notifications = new Notifications(this);
+		Timeout timeout = new Timeout(scheduledService, 300, TimeUnit.SECONDS);
+		
+		compositeStationListener.addListener(new ToastStationListener(notifications));
+		compositeStationListener.addListener(new NotificationStationListener(notifications, timeout));
+		
+		return rootListener;
 	}
 	
 	private TelephonyManager getTelephonyManager() {
@@ -129,6 +88,7 @@ public class NotificationService extends Service {
     	Log.v(LOG_NAME, "Starting service...");
     	TelephonyManager tm = getTelephonyManager();
     	if (tm != null) {
+    		rootListener = buildListeners();
     		tm.listen(new StateListener(), PhoneStateListener.LISTEN_SERVICE_STATE);
     		Log.v(LOG_NAME, "Listener registered");
     	} else {
